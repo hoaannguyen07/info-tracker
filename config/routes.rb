8b848369fd5d42a698cb37b7ed848abf67ef4ca4
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-09-24 15:01:33 -0500

    implemented google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  root 'users#main'
+    root to: 'dashboards#show'
+    devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+    devise_scope :admin do
+      get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+      get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+    end
+#   root 'login#index'
 
-  get 'login/index'
-  get 'users/main'
-  get 'users/notes'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'login/index'
+    get 'users/main'
+    get 'users/notes'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #default route
-  get ':controller(/:action(/:id))'
+    #default route
+    get ':controller(/:action(/:id))'
 
   
 end
