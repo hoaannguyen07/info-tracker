750f2b0a66e61d85cbbb579aa94fea22e546b48e
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-10-15 00:05:11 -0500

    updated tests to reflect changein column players col name and validation
    updated test cases to use 'losses' instead of 'played' and took out comparison between wins & losses.
    
    Linted code to have no to_do for rubocop anymore.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,76 +1,76 @@
 # frozen_string_literal: true
 
 class PlayersController < ApplicationController
-  before_action :set_player, only: %i[show edit update destroy]
+    before_action :set_player, only: %i[show edit update destroy]
 
-  # GET /players or /players.json
-  def index
-    user = Admin.where(email: current_admin.email).first
-    @players = Player.where(admin_id: user.id).order('name ASC')
-  end
-
-  # GET /players/1 or /players/1.json
-  def show; end
-
-  # GET /players/new
-  def new
-    @player = Player.new(losses: 0, wins: 0)
-  end
-
-  # GET /players/1/edit
-  def edit; end
-
-  # POST /players or /players.json
-  def create
-    @player = Player.new(player_params)
-    # need to add admin id for the new player that is being created to indicate ownership of that player information
-    # and let this user and only this user have access to that information when querying
-    user = Admin.where(email: current_admin.email).first
-    @player.admin_id = user.id
-
-    respond_to do |format|
-      if @player.save
-        format.html { redirect_to(@player, notice: 'Player was successfully created.') }
-        format.json { render(:show, status: :created, location: @player) }
-      else
-        format.html { render(:new, status: :unprocessable_entity) }
-        format.json { render(json: @player.errors, status: :unprocessable_entity) }
-      end
+    # GET /players or /players.json
+    def index
+        user = Admin.where(email: current_admin.email).first
+        @players = Player.where(admin_id: user.id).order('name ASC')
     end
-  end
 
-  # PATCH/PUT /players/1 or /players/1.json
-  def update
-    # no need to udpate admin_id b/c it is already there
-    respond_to do |format|
-      if @player.update(player_params)
-        format.html { redirect_to(@player, notice: 'Player was successfully updated.') }
-        format.json { render(:show, status: :ok, location: @player) }
-      else
-        format.html { render(:edit, status: :unprocessable_entity) }
-        format.json { render(json: @player.errors, status: :unprocessable_entity) }
-      end
+    # GET /players/1 or /players/1.json
+    def show; end
+
+    # GET /players/new
+    def new
+        @player = Player.new(losses: 0, wins: 0)
     end
-  end
 
-  # DELETE /players/1 or /players/1.json
-  def destroy
-    @player.destroy!
-    respond_to do |format|
-      format.html { redirect_to(players_url, notice: 'Player was successfully destroyed.') }
-      format.json { head(:no_content) }
+    # GET /players/1/edit
+    def edit; end
+
+    # POST /players or /players.json
+    def create
+        @player = Player.new(player_params)
+        # need to add admin id for the new player that is being created to indicate ownership of that player information
+        # and let this user and only this user have access to that information when querying
+        user = Admin.where(email: current_admin.email).first
+        @player.admin_id = user.id
+
+        respond_to do |format|
+            if @player.save
+                format.html { redirect_to(@player, notice: 'Player was successfully created.') }
+                format.json { render(:show, status: :created, location: @player) }
+            else
+                format.html { render(:new, status: :unprocessable_entity) }
+                format.json { render(json: @player.errors, status: :unprocessable_entity) }
+            end
+        end
     end
-  end
 
-  private
+    # PATCH/PUT /players/1 or /players/1.json
+    def update
+        # no need to udpate admin_id b/c it is already there
+        respond_to do |format|
+            if @player.update(player_params)
+                format.html { redirect_to(@player, notice: 'Player was successfully updated.') }
+                format.json { render(:show, status: :ok, location: @player) }
+            else
+                format.html { render(:edit, status: :unprocessable_entity) }
+                format.json { render(json: @player.errors, status: :unprocessable_entity) }
+            end
+        end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_player
-    @player = Player.find(params[:id])
-  end
+    # DELETE /players/1 or /players/1.json
+    def destroy
+        @player.destroy!
+        respond_to do |format|
+            format.html { redirect_to(players_url, notice: 'Player was successfully destroyed.') }
+            format.json { head(:no_content) }
+        end
+    end
 
-  # Only allow a list of trusted parameters through.
-  def player_params
-    params.require(:player).permit(:name, :losses, :wins, :strengths, :weaknesses, :additional_info)
-  end
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_player
+        @player = Player.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def player_params
+        params.require(:player).permit(:name, :losses, :wins, :strengths, :weaknesses, :additional_info)
+    end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,31 +2,29 @@
 
 # https://api.rubyonrails.org/v5.1.7/classes/ActiveModel/Validations/ClassMethods.html
 class Player < ApplicationRecord
-  belongs_to :admin
+    belongs_to :admin
 
-  validates :admin_id, :name, :losses, :wins, presence: true
+    validates :admin_id, :name, :losses, :wins, presence: true
 
-  # name cannot have any special characters besides '-' and '_' and spaces
-  validates :name,
-            format: {
-              with: /\A[A-Za-z\d\-_ ]*\z/,
-              message: "Cannot use any special characters other than spaces, '-' (dashes), or '_' (underscores)"
-            }
+    # name cannot have any special characters besides '-' and '_' and spaces
+    validates :name,
+              format: {
+                  with: /\A[A-Za-z\d\-_ ]*\z/,
+                  message: "Cannot use any special characters other than spaces, '-' (dashes), or '_' (underscores)"
+              }
 
-  # played & wins needs to be a positive integer that is within integer bounds
-  validates :losses, :wins, numericality: {
-    only_integer: true,
-    greater_than_or_equal_to: 0,
-    less_than_or_equal_to: 2_147_483_647,
-    message: 'values must be between 0 and 2,147,483,647'
-  }
+    # played & wins needs to be a positive integer that is within integer bounds
+    validates :losses, :wins, numericality: {
+        only_integer: true,
+        greater_than_or_equal_to: 0,
+        less_than_or_equal_to: 2_147_483_647,
+        message: 'values must be between 0 and 2,147,483,647'
+    }
 
-  # can't have wins > played b/c you can't win without playing
-  # validate :played_at_least_equal_wins
+    # can't have wins > played b/c you can't win without playing
+    # validate :played_at_least_equal_wins
 
-  private
-
-  # def played_at_least_equal_wins
-  #   errors.add(:played, 'cannot be less than wins') unless wins && played && played >= wins
-  # end
+    # def played_at_least_equal_wins
+    #   errors.add(:played, 'cannot be less than wins') unless wins && played && played >= wins
+    # end
 end
