6449dcbf43e40cbefbab9d6c23766f748492895e
Brian B. Shao <brianshao9@gmail.com>
2021-10-19 10:13:47 -0500

    Adding changes done for unit testing for event model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :events
     root to: 'players#index'
     devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
     devise_scope :admin do
@@ -11,17 +9,8 @@ Rails.application.routes.draw do
     end
     #   root 'login#index'
     resources :players
-=======
-  root to: 'players#index'
-  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
-  devise_scope :admin do
-    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
-    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
-  end
-  #   root 'login#index'
-  resources :players
->>>>>>> 41eab3a2eb57f1fb59545019de2fe27c8d7b5abc
-
+    resources :events
+    
   get 'login/index'
   get 'users/main'
   get 'users/notes'
