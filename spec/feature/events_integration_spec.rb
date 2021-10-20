34c0834c1aa3ba2cfe8e4363c581778cfabb72e0
Brian B. Shao <brianshao9@gmail.com>
2021-10-19 19:10:39 -0500

    start of integration test of events

---
+++ spec/feature/events_integration_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# location: spec/feature/events_integration_spec.rb
+require 'rails_helper'
+require 'selenium-webdriver'
+
+RSpec.describe('Players Features', type: :feature) do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
+    visit root_path
+    # sign in and verify sign in
+    click_on 'Get Started!'
+
+    # start with fresh player database on every run
+    Player.delete_all
+    visit events_path
+  end
+
+end
