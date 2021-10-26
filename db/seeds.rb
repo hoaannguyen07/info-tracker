3d1ea1c029f6d907b1c92f09df800b015d898c1f
root <root@localhost>
2021-10-26 01:20:15 +0000

    seeded admins and updated Procfile to seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,5 @@ end
 
 seed_admin('matrev@tamu.edu', 'Mark Trevino', '117972233671837615000', 'https://lh3.googleusercontent.com/a/AATXAJyTPeVQNjmwW71cuYem7Msi_KpuKOD0huwG1iBB=s96-c')
 seed_admin('hoaannguyen07@gmail.com', 'hoa nguyen', '116423861261808432463', 'https://lh3.googleusercontent.com/a-/AOh14Gi1_Nq0U3QdPz9BM5iTF9FOvg0tBnBRblEqWVNV=s96-c')
+seed_admin('brianshao@tamu.edu', 'Brian Shao', '108918496416922689353', 'https://lh3.googleusercontent.com/a/AATXAJwaibfLd5CxDce5m8DcVBlLbWqNJ1yFCE7-Rk80=s96-c%22')
+seed_admin('latulod@tamu.edu', 'Leonardo Tulod', '103458809952587783049', 'https://lh3.googleusercontent.com/a/AATXAJxpjAGAio9MM6H2Zjkhs2CUSYNGDVaVu_4PgJVZ=s96-c')
