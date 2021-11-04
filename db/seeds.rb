d0de19cd73189e87c4b622c707a55e312fa92c62
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-11-03 19:26:58 -0500

    Update seeds.rb
    seeded ian

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,4 @@ seed_admin('matrev@tamu.edu', 'Mark Trevino', '117972233671837615000', 'https://
 seed_admin('hoaannguyen07@gmail.com', 'hoa nguyen', '116423861261808432463', 'https://lh3.googleusercontent.com/a-/AOh14Gi1_Nq0U3QdPz9BM5iTF9FOvg0tBnBRblEqWVNV=s96-c')
 seed_admin('brianshao@tamu.edu', 'Brian Shao', '108918496416922689353', 'https://lh3.googleusercontent.com/a/AATXAJwaibfLd5CxDce5m8DcVBlLbWqNJ1yFCE7-Rk80=s96-c%22')
 seed_admin('latulod@tamu.edu', 'Leonardo Tulod', '103458809952587783049', 'https://lh3.googleusercontent.com/a/AATXAJxpjAGAio9MM6H2Zjkhs2CUSYNGDVaVu_4PgJVZ=s96-c')
+seed_admin('ianmstephenson17@gmail.com', 'Ian Stephenson', '109075461371842403736', 'https://lh3.googleusercontent.com/a/AATXAJwBURI0d_d0Y6Vzf3fKmUt49xdSnGHAcOzAKzCd=s96-c')
