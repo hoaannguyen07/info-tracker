4a4c6c1361a331ef26691e038036f209a756549b
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-11-09 18:47:44 -0600

    updated sidebar.js, denied permission, and test
    updated sidebar to show active tab based on url path (previously a bug).
    
    updated Permissions Denied Page and tests accordingly.

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -27,19 +27,33 @@ const toggleNavbar = (toggle, nav, headerpd) =>{
 	}
 }
 
-function setCurrentTab(navLinkColor)
-{
-	// set cur_tab if there are no save cur_tab or if cur_tab is empty/undefined
-	if (localStorage.length == 0 || !localStorage.getItem('cur_tab'))
-	{
+function setLocalStorageCurrentPath() {
+	const cur_path = window.location.pathname;
+	if (cur_path.includes('events')) {
+		localStorage.setItem('cur_tab', 'events-tab');
+	} else if (cur_path.includes('images')) {
+		localStorage.setItem('cur_tab', 'gallery-tab');
+	} else if (cur_path.includes('profile')) {
+		localStorage.setItem('cur_tab', 'profile-tab');
+	} else if (cur_path.includes('permission')) {
+		localStorage.setItem('cur_tab', 'manage-users-tab');
+	} else { // root tab or home tab will show home tab as active
 		localStorage.setItem('cur_tab', 'home-tab');
 	}
+}
+
+function setCurrentTabActive(navLinkColor)
+{
+	setLocalStorageCurrentPath();
 
 	// load the current active tab (home tab when just logged in)
 	if(navLinkColor){
 		navLinkColor.forEach(l=> l.classList.remove('active'))
 		const cur_tab = document.getElementById(localStorage.getItem('cur_tab'));
-		cur_tab.classList.add('active');
+		// only add to classList of cur_tab if it exists
+		if (cur_tab) {
+			cur_tab.classList.add('active');
+		}
 	}
 }
 
@@ -51,7 +65,7 @@ function onReady() {
 	toggleNavbar(toggle, nav, headerpd);
 	
 	const navLinkColor = document.querySelectorAll('.tab')
-	setCurrentTab(navLinkColor);
+	setCurrentTabActive(navLinkColor);
 
 	/*===== LINK ACTIVE =====*/
 	function colorLink() {
@@ -59,7 +73,7 @@ function onReady() {
 			// delete all active tabs
 			navLinkColor.forEach(l => l.classList.remove('active'));
 			// get the tab that is clicked on and save it localled for reloading
-			const cur_tab = (this.id != 'root-tab') ? this.id : 'home-tab';
+			const cur_tab = (this.id != 'root-tab') ? this.id : 'home-tab'; // root tab is home tab
 			localStorage.setItem('cur_tab', cur_tab);
 
 			// add "active" class to tab being clicked on to get active to appear sooner visually though it will load again after
